aed8b3d73678f12b1400efefc595ad0c17d63a09
Thibaut Jombart <thibautjombart@gmail.com>
2017-05-28 11:47:30 +0100

    adding sampling

---
+++ R/sample_R.R
@@ -0,0 +1,35 @@
+#' Get a sample of plausible Reproduction Numbers
+#'
+#' This function derives a sample of plausible R values from an \code{earlyR}
+#' object (as returned by \code{\link{get_R}}).
+#'
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @export
+#'
+#' @examples
+#'
+#' if (require(incidence)) {
+#'  x <- incidence(c(1, 5, 5, 12, 45, 65))
+#'  plot(x)
+#'  res <- get_R(x)
+#'  res
+#'  plot(res)
+#'
+#'  sample_R(res, 10)
+#'  hist(sample_R(res, 1000), col = "grey", border = "white")
+#' }
+#'
+#' @param x An \code{earlyR} object.
+#'
+#' @param n The number of R values to sample.
+#'
+
+sample_R <- function(x, n = 100) {
+  if (!inherits(x, "earlyR")) {
+    stop("x is not an earlyR object")
+  }
+
+  sample(x$R_grid, prob = x$R_like, n, replace = TRUE)
+}
